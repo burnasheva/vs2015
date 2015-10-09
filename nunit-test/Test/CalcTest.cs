964be2a5a908e22e887692a63040e73f1bebe3b2
unknown <Nadezhda.Burnasheva@unit-447.Labs.IntelliJ.Net>
2015-10-09 16:38:31 +0300

    first commit

---
+++ nunit-test/Test/CalcTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace nunit_test.Test
+{
+    [TestFixture]
+    class CalcTest
+    {
+        [Test]
+        public void minus_should_work()
+        {
+            Assert.That(4 - 2, Is.EqualTo(2));
+        }
+    }
+}
