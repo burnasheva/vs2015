72b272015349f549477f6df011680187f7660686
unknown <Nadezhda.Burnasheva@unit-447.Labs.IntelliJ.Net>
2015-10-09 16:54:48 +0300

    add 2 test classes

---
+++ nunit-test/Test/AbcTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace nunit_test.Test
+{
+    [TestFixture]
+    class AbcTest
+    {
+        [Test]
+        public void test_smth()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ nunit-test/Test/MultiplyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace nunit_test.Test
+{
+    [TestFixture]
+    class MultiplyTest
+    {
+        [Test]
+        public void multiply_should_work()
+        {
+            Assert.That(2 * 4, Is.EqualTo(8));
+        }
+    }
+}
