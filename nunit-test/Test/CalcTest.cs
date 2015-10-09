2af82809c9d69c411c9121df56f084e05e276b92
unknown <Nadezhda.Burnasheva@unit-447.Labs.IntelliJ.Net>
2015-10-09 16:45:07 +0300

    add more tests

--- nunit-test/Test/CalcTest.cs
+++ nunit-test/Test/CalcTest.cs
@@ -10,5 +10,17 @@ namespace nunit_test.Test
         {
             Assert.That(4 - 2, Is.EqualTo(2));
         }
+
+        [Test]
+        public void plus_should_work()
+        {
+            Assert.That(4 + 2, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void minus_should_not_work()
+        {
+            Assert.That(4 - 2, Is.GreaterThan(0));
+        }
     }
 }
